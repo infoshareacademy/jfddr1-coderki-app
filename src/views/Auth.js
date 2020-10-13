658eff30ca2d3d37d2a302c5b3c2b27efcf5abaa
Monika Wieczorek <cendrowska.monika@gmail.com>
2020-10-13 23:05:54 +0200

    WIP static views

--- src/views/Auth.js
+++ src/views/Auth.js
@@ -1,16 +1,46 @@
 import React from 'react';
+import styles from './Auth.module.css';
 
-const Auth = () => (
-  <div>
-    <p>To jest strona logowania</p>
-    <button
-      onClick={() => {
-        console.log('klik');
-      }}
-    >
-      Klik
-    </button>
-  </div>
-);
+const Header = () => {
+  return (
+    <div>
+      <h2>Home Manager</h2>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div>
+      <h2>This will be a footer</h2>
+    </div>
+  );
+};
+
+const Auth = () => {
+  return (
+    <div className={styles.container}>
+      <Header />
+      <form className={styles.authForm}>
+        <label>Email</label>
+        <input type="email" name="email" />
+        <label>Password</label>
+        <input type="password" name="password" />
+        <label>Confirm password</label>
+        <input type="password" name="password" />
+        <button>Login</button>
+      </form>
+      <div>
+        <p>
+          You do not have an account?
+          <a href="#" target="_blank">
+            Register
+          </a>
+        </p>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Auth;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
-const Home = () => <div>To jest główna strona aplikacji</div>;
+const Navigation = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+      <a href="#">Home</a>
+      <a href="#">Calendar</a>
+      <a href="#">Tasks</a>
+      <a href="#">Settings</a>
+    </div>
+  );
+};
+
+const Home = () => {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+};
 
 export default Home;
