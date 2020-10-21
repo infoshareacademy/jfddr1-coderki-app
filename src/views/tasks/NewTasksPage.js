1dd9486529f1d9be818222a1a3586010aa7d0659
Monika Wieczorek <cendrowska.monika@gmail.com>
2020-10-21 16:22:16 +0200

    commented WIP firebase

--- src/views/tasks/NewTasksPage.js
+++ src/views/tasks/NewTasksPage.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import TasksDatabase from '../../Firebase/firebase';
+// import React from 'react';
+// import TasksDatabase from '../../Firebase/firebase';
 
-// import styles from './TasksPage.module.css';
+// // import styles from './TasksPage.module.css';
 
-const collection = TasksDatabase.collection('tasks')
-  .get()
-  .then((tasks) => {
-    tasks.forEach((task) => {
-      console.log(task.id);
-      console.log(task.data());
-    });
-  });
+// const collection = TasksDatabase.collection('tasks')
+//   .get()
+//   .then((tasks) => {
+//     tasks.forEach((task) => {
+//       console.log(task.id);
+//       console.log(task.data());
+//     });
+//   });
 
-const NewTasksPage = () => {
-  return <div>{collection}</div>;
-};
+// const NewTasksPage = () => {
+//   return <div>{collection}</div>;
+// };
 
-export default NewTasksPage;
+// export default NewTasksPage;
