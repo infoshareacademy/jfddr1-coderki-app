ae7cf69df89029427fb83d432424d966ad13b144
Monika Wieczorek <cendrowska.monika@gmail.com>
2020-10-21 22:38:11 +0200

    Add tasks to Firebase resolved

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -10,6 +10,20 @@ var firebaseApp = firebase.initializeApp({
   appId: '1:801523551576:web:1fd3be0123da8430ef692d',
 });
 
-const TasksDatabase = firebaseApp.firestore();
+const db = firebaseApp.firestore();
 
-export default TasksDatabase;
+export default db;
+
+// const config = {
+//   apiKey: 'AIzaSyCmO0JAsTO83g3Io6kyvYVu3wEsiQ7oY98',
+//   authDomain: 'coderki-app.firebaseapp.com',
+//   databaseURL: 'https://coderki-app.firebaseio.com',
+//   projectId: 'coderki-app',
+//   storageBucket: 'coderki-app.appspot.com',
+//   messagingSenderId: '801523551576',
+//   appId: '1:801523551576:web:1fd3be0123da8430ef692d',
+// };
+
+// firebase.initializeApp(config);
+
+// export default firebase;
--- src/views/tasks/NewTasksPage.js
+++ src/views/tasks/NewTasksPage.js
@@ -1,19 +1,57 @@
-// import React from 'react';
-// import TasksDatabase from '../../Firebase/firebase';
+import React from 'react';
+// import firebase from 'firebase/app';
+import db from '../../Firebase/firebase';
 
-// // import styles from './TasksPage.module.css';
+class Task extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      place: '',
+    };
+  }
 
-// const collection = TasksDatabase.collection('tasks')
-//   .get()
-//   .then((tasks) => {
-//     tasks.forEach((task) => {
-//       console.log(task.id);
-//       console.log(task.data());
-//     });
-//   });
+  updateInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-// const NewTasksPage = () => {
-//   return <div>{collection}</div>;
-// };
+  addUser = (e) => {
+    e.preventDefault();
+    db.collection('tasks').add({
+      title: this.state.title,
+      place: this.state.place,
+    });
+    this.setState({
+      title: '',
+      place: '',
+    });
+  };
 
-// export default NewTasksPage;
+  render() {
+    return (
+      <>
+        <h1 style={{ textAlign: 'center' }}>Hello :)</h1>
+        <form onSubmit={this.addUser}>
+          <input
+            type="text"
+            name="title"
+            placeholder="Task name"
+            onChange={this.updateInput}
+            value={this.state.title}
+          />
+          <input
+            type="text"
+            name="place"
+            placeholder="Place"
+            onChange={this.updateInput}
+            value={this.state.place}
+          />
+          <button type="submit">CREATE TASK</button>
+        </form>
+      </>
+    );
+  }
+}
+export default Task;
