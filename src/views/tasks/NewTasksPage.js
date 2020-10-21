2725dd9565f7f3548b14198b239e7a5b210c0ac0
Monika Wieczorek <cendrowska.monika@gmail.com>
2020-10-21 15:28:13 +0200

    Firebase in configuration

--- src/views/tasks/NewTasksPage.js
+++ src/views/tasks/NewTasksPage.js
@@ -1,12 +1,19 @@
 import React from 'react';
 // import styles from './TasksPage.module.css';
 
+const TasksDatabase = firebase
+  .firestore()
+  .collection('tasks')
+  .get()
+  .then((tasks) => {
+    tasks.forEach((task) => {
+      console.log(task.id);
+      console.log(task.data());
+    });
+  });
+
 const NewTasksPage = () => {
-  return (
-    <div>
-      <h2>Dodaj nowy task</h2>
-    </div>
-  );
+  return <div>Hello</div>;
 };
 
 export default NewTasksPage;
