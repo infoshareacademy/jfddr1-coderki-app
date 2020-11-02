4115ddf008580e51a5565cb01cd4d328065cabfb
Monika Wieczorek <cendrowska.monika@gmail.com>
2020-11-02 14:29:31 +0100

    removal of excess code

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Menu from './components/Menu';
 import Header from './components/Header';
 import HomePage from './views/home/HomePage';
