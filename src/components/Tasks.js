2aceb49ea7fef2363de36bc873f86180ac673c3e
Aleksandra Rybinska <rybinska.aleksandra@gmail.com>
2020-11-03 14:25:36 +0100

    task details on home page - WIP

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Task.module.css';
 import dots from '../img/horizontal-dots.svg';
 
 export const Tasks = ({ tasks }) => {
+  const [showTaskDetails, setShowTaskDetails] = useState(false);
+  const [selectedTaskId, setSelectedTaskId] = useState(null);
+
+  const handleToggle = (id) => {
+    selectedTaskId === id ? setSelectedTaskId(null) : setSelectedTaskId(id);
+  };
+
   return (
-    <div style={{ paddingTop: 15 }}>
-      {tasks.map((task) => (
-        <>
-          <div className={styles.mainContainer}>
-            <div className={styles.textContainer}>
-              <p className={styles.smallerText}>
-                {task.startTimeData}
-                <span style={{ marginLeft: 15 }}>{task.startTimeTime}</span>
-              </p>
-              <p className={styles.titleText}>{task.title}</p>
-              <p className={styles.smallerText}>{task.place}</p>
+    <>
+      <div style={{ paddingTop: 15 }}>
+        {tasks.map((task) => (
+          <>
+            <div
+              onClick={(id) => handleToggle(id)}
+              className={styles.mainContainer}
+            >
+              <div className={styles.textContainer}>
+                <p className={styles.smallerText}>
+                  {task.startTimeData}
+                  <span style={{ marginLeft: 15 }}>{task.startTimeTime}</span>
+                </p>
+                <p className={styles.titleText}>{task.title}</p>
+                <p className={styles.smallerText}>{task.place}</p>
+              </div>
+              <div className={styles.iconContainer}>
+                <img
+                  src={dots}
+                  className={styles.seeMoreIcon}
+                  alt="icon with 3 dots"
+                />
+              </div>
             </div>
-            <div className={styles.iconContainer}>
-              <img
-                src={dots}
-                className={styles.seeMoreIcon}
-                alt="icon with 3 dots"
-              />
-            </div>
-          </div>
-        </>
-      ))}
-    </div>
+            {task.id === selectedTaskId && (
+              <div>
+                <h2>tu są detale</h2>
+              </div>
+            )}
+          </>
+        ))}
+      </div>
+    </>
   );
 };
