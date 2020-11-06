9f46d22a701100326b2d6a0782744707da7db506
Aleksandra Rybinska <rybinska.aleksandra@gmail.com>
2020-11-06 15:47:45 +0100

    init react-big-calendar, moment

--- src/views/calendar/CalendarPage.js
+++ src/views/calendar/CalendarPage.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
 
-const CalendarPage = () => <div>To jest Kalendarz</div>;
+// const localizer = momentLocalizer(moment);
+
+const CalendarPage = () => {
+  return (
+    <>
+      <div>To jest Kalendarz</div>
+      {/* <div>
+        <Calendar
+          localizer={localizer}
+          //   events={myEventsList}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+        />
+      </div> */}
+    </>
+  );
+};
 
 export default CalendarPage;
